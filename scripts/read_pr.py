fd42c31ed2e77587f68c1325604ecd83f3c1e151
kobakobashu <kobaya4@fuji.waseda.jp>
2021-10-29 15:19:25 +0900

    <issue> #6 Send Pull Request titles to Slack
    <type> refactor
    
    <content> refactor: use format, make writer class and so on

--- scripts/read_pr.py
+++ scripts/read_pr.py
@@ -2,33 +2,43 @@ import argparse
 import logging
 import sqlite3
 
+import write
+
 from config import DB_ROOT
 
 
-def read_pr(db: str, slack_user_id: str, repo: str):
-  """return pr titles
+def read_pr(db_path: str, slack_user_id: str, repository_name: str) -> None:
+  """return PR titles
 
   Args:
-    db(str): pass to the git_slack.db
+    db_path(str): path to the git_slack.db
     slack_user_id(str): slack user id
-    repo(str): repository name
+    repository_name(str): repository name
   """
-  with sqlite3.connect(db) as con:
+  with sqlite3.connect(db_path) as con:
     cursor = con.cursor()
     cursor.execute('SELECT github_name FROM usernames WHERE slack_user_id = ?', slack_user_id)
     github_user_entry = cursor.fetchone()
+
     if not github_user_entry:
-      logger.warning('Unregistered Github username')
+      msg = 'Unregistered Github username'
+      log_level = "warning"
       con.close()
 
-    cursor.execute('SELECT pr_title, pr_number, pr_url FROM pull_requests WHERE pr_reviewer = ? AND repo_name = ?', (github_user_entry, repo))
+    cursor.execute('SELECT pr_title, pr_number, pr_url FROM pull_requests WHERE pr_reviewer = ? AND repo_name = ?', (github_user_entry, repository_name))
     pr_titles = cursor.fetchall()
 
     if not pr_titles:
-      logger.info('No Pull Request to review')
+      msg = 'No Pull Request to review'
+      log_level = "info"
       con.close()
-      
-    logger.info(pr_titles)
+
+    msg = pr_titles
+    log_level = "info"
+
+    writer = write.MultipleWriter()
+    writer.write(msg, log_level)
+  
     con.close()
 
 
@@ -36,11 +46,12 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("-param1", type=str, default=None, help="slack_user")
   parser.add_argument("-param2", type=str, default=None, help="repository")
-  args = parser.parse_args()
-  slack_user_id = args[0]
-  repo = args[1]
-  DB_NAME = "git_slack.db"
-  db = f'{DB_ROOT}/{DB_NAME}'
-  logging.basicConfig(level=logging.INFO)
+  slack_user_id, repository_name = parser.parse_args()[:2]
+
+  db_path = "{DB_ROOT}/{DB_NAME}".format(DB_ROOT=DB_ROOT, DB_NAME="git_slack.db")
+  log_path = "{DB_ROOT}/{LOG_NAME}".format(DB_ROOT=DB_ROOT, LOG_NAME="logger.log")
+
+  logging.basicConfig(filename=log_path, level=logging.INFO)
   logger = logging.getLogger(__name__)
-  read_pr(db, slack_user_id, repo)
+
+  read_pr(db_path, slack_user_id, repository_name)
