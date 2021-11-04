2bd0dc50960794072831c26baa8988844089b05a
kobakobashu <kobaya4@fuji.waseda.jp>
2021-11-04 16:45:31 +0900

    <issue> #5 Register Slack username and Github username in the database
    <type> refactor
    
    <content> refactor: use format, add config

--- scripts/write.py
+++ scripts/write.py
@@ -1,17 +1,17 @@
 import logging
 
-from config import DB_ROOT
+from config import LOG_PATH
 
 
-log_path = "{DB_ROOT}/{LOG_NAME}".format(DB_ROOT=DB_ROOT, LOG_NAME="logger.log")
+log_path = LOG_PATH.format(LOG_NAME="logger.log")
 logging.basicConfig(filename=log_path, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class MultipleWriter:
-  def write(self, msg: str, log_level: str) -> None:
-    if msg:
-      print(msg)
-    if log_level == "info":
-      logger.info(msg)
-    elif log_level == "warning":
-      logger.warning(msg)
+    def write(self, msg: str, log_level: str) -> None:
+        if msg:
+            print(msg)
+        if log_level == "info":
+            logger.info(msg)
+        elif log_level == "warning":
+            logger.warning(msg)
