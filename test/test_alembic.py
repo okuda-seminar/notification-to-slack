38c5ec46f0029cb04bcda4ae801e5c8e9771b830
tkkawa <tkm.kawa@gmail.com>
2021-11-08 15:10:13 +0900

    remove magic number and add config

--- test/test_alembic.py
+++ test/test_alembic.py
@@ -1,21 +1,25 @@
 import sqlite3
 import unittest
 
-from config import DB_ROOT
+from config import DB_PATH
 
 
 class TestAlembic(unittest.TestCase):
     def test_create_table(self):
 
-        db_path = "{DB_ROOT}/{DB_NAME}".format(DB_ROOT=DB_ROOT, DB_NAME="git_slack.db")
+        db_path = DB_PATH.format(DB_NAME="git_slack.db")
         with sqlite3.connect(db_path) as con:
             cursor = con.cursor()
 
             table = cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""").fetchall()
 
-            self.assertEqual(table[1][0], "repositories")
-            self.assertEqual(table[2][0], "usernames")
-            self.assertEqual(table[3][0], "pull_requests")
+            REPOSITORIES_IDX = 1
+            USERNAMES_IDX = 2
+            PULL_REQUESTS_IDX = 3
+
+            self.assertEqual(table[REPOSITORIES_IDX][0], "repositories")
+            self.assertEqual(table[USERNAMES_IDX][0], "usernames")
+            self.assertEqual(table[PULL_REQUESTS_IDX][0], "pull_requests")
 
 
 if __name__ == "__main__":
