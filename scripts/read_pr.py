d4423e605df8a37fe870e2e9e400637e6301a839
kobakobashu <kobaya4@fuji.waseda.jp>
2021-11-22 16:37:50 +0900

    <issue> #6 add read_pr.py and send_pr.coffee
    <type> bug modify

--- scripts/read_pr.py
+++ scripts/read_pr.py
@@ -1,57 +1,55 @@
 import argparse
-import logging
 import sqlite3
 
-import write
+from write import MultipleWriter
 
-from config import DB_ROOT
+from config import DB_PATH
 
 
 def read_pr(db_path: str, slack_user_id: str, repository_name: str) -> None:
-  """return PR titles
+    """Return PR titles
+    Args:
+      db_path(str): path to the git_slack.db
+      slack_user_id(str): slack user id
+      repository_name(str): repository name
+    """
+    with sqlite3.connect(db_path) as con:
+        cursor = con.cursor()
+        usernames = cursor.execute("SELECT * FROM usernames").fetchall()
+        cursor.execute(
+            'SELECT github_name FROM usernames WHERE slack_user_id = ?', (slack_user_id,)
+        )
+        github_user_entry = cursor.fetchone()
 
-  Args:
-    db_path(str): path to the git_slack.db
-    slack_user_id(str): slack user id
-    repository_name(str): repository name
-  """
-  with sqlite3.connect(db_path) as con:
-    cursor = con.cursor()
-    cursor.execute('SELECT github_name FROM usernames WHERE slack_user_id = ?', slack_user_id)
-    github_user_entry = cursor.fetchone()
+        if not github_user_entry:
+            msg = 'Unregistered Github username'
+            log_level = "warning"
 
-    if not github_user_entry:
-      msg = 'Unregistered Github username'
-      log_level = "warning"
-      con.close()
+        cursor.execute(
+            'SELECT pr_title, pr_number, pr_url FROM pull_requests WHERE pr_reviewer = ?', (github_user_entry)
+        )
+        pr_titles = cursor.fetchall()
 
-    cursor.execute('SELECT pr_title, pr_number, pr_url FROM pull_requests WHERE pr_reviewer = ? AND repo_name = ?', (github_user_entry, repository_name))
-    pr_titles = cursor.fetchall()
+        if len(pr_titles) == 0:
+              msg = 'No Pull Request to review'
+              log_level = "info"
+    
+        if not msg:
+              msg = pr_titles
+              log_level = "info"
 
-    if not pr_titles:
-      msg = 'No Pull Request to review'
-      log_level = "info"
-      con.close()
-
-    msg = pr_titles
-    log_level = "info"
-
-    writer = write.MultipleWriter()
-    writer.write(msg, log_level)
-  
-    con.close()
+        writer.write(msg, log_level)
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-param1", type=str, default=None, help="slack_user")
-  parser.add_argument("-param2", type=str, default=None, help="repository")
-  slack_user_id, repository_name = parser.parse_args()[:2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-param1", type=str, default=None, help="slack_user")
+    parser.add_argument("-param2", type=str, default=None, help="repository")
+    args = parser.parse_args()
+    slack_user_id, repository_name = args.param1, args.param2
 
-  db_path = "{DB_ROOT}/{DB_NAME}".format(DB_ROOT=DB_ROOT, DB_NAME="git_slack.db")
-  log_path = "{DB_ROOT}/{LOG_NAME}".format(DB_ROOT=DB_ROOT, LOG_NAME="logger.log")
+    db_path = DB_PATH.format(DB_NAME="git-slack.db")
 
-  logging.basicConfig(filename=log_path, level=logging.INFO)
-  logger = logging.getLogger(__name__)
+    writer = MultipleWriter()
 
-  read_pr(db_path, slack_user_id, repository_name)
+    read_pr(db_path, slack_user_id, repository_name)
--- scripts/send_pr.coffee
+++ scripts/send_pr.coffee
@@ -1,34 +1,25 @@
 module.exports = (robot) ->
-
-  class PullRequest
-    filter_reviewer: (slack_user_id, repo) ->
-      {PythonShell} = require('python-shell');
-
-      execute_path = './scripts/read_pr.py'
-      options = {
-          args:[
-              '-param1', slack_user_id
-              '-param2', repo
-          ]
-      }
-
-      pyshell = new PythonShell(execute_path, options);
-      pyshell.send()
-      pyshell.on('message', (pr_titles)->
-        msg.send pr_titles
-      )
-
-
-  main = (slack_user_id, repo) ->
-    pr_titles = new PullRequest().filter_reviewer(slack_user_id, repo)
-    return pr_titles
-
-  github = require("githubot")(robot)
-
-  unless (url_api_base = process.env.HUBOT_GITHUB_API)?
-    url_api_base = "https://api.github.com"
-
   robot.respond /showpr\s+(me\s+)?(.*)\s+pulls(\s+with\s+)?(.*)?/i, (msg)->
-    repo = github.qualified_repo msg.match[2]
+    github = require("githubot")(robot)
+
+    unless (url_api_base = process.env.HUBOT_GITHUB_API)?
+      url_api_base = "https://api.github.com"
+
     slack_user_id = msg.message.user.id;
-    main(slack_user_id, repo)
+    repo = github.qualified_repo msg.match[2]
+
+    {PythonShell} = require('python-shell');
+
+    execute_path = './scripts/read_pr.py'
+    options = {
+      args:[
+        '-param1', slack_user_id
+        '-param2', repo
+      ]
+    };
+
+    pyshell = new PythonShell(execute_path, options);
+    pyshell.send()
+    pyshell.on('message', (message)->
+      msg.send message
+    );
