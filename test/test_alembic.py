e999156c80bc6caf7d1a6a4fcdaababd5a5428f0
tkkawa <tkm.kawa@gmail.com>
2021-11-22 15:41:12 +0900

    add enum

--- test/test_alembic.py
+++ test/test_alembic.py
@@ -1,12 +1,18 @@
+from enum import Enum
 import sqlite3
 import unittest
-
-import pandas as pd
 
 from config import DB_PATH
 
 
+class Tables(Enum):
+    REPOSITORIES = 1
+    USERNAMES = 2
+    PULL_REQUESTS = 3
+
+
 class TestAlembic(unittest.TestCase):
+
     def test_create_table(self):
 
         db_path = DB_PATH.format(DB_NAME="git_slack.db")
@@ -15,13 +21,9 @@ class TestAlembic(unittest.TestCase):
 
             table = cursor.execute("""SELECT name FROM sqlite_master WHERE type='table';""").fetchall()
 
-            idx_df = pd.DataFrame(
-                    dict(REPOSITORIES_IDX=[1], USERNAMES_IDX=[2], PULL_REQUESTS_IDX=[3])
-                    )
-
-            self.assertEqual(table[idx_df.REPOSITORIES_IDX.values[0]][0], "repositories")
-            self.assertEqual(table[idx_df.USERNAMES_IDX.values[0]][0], "usernames")
-            self.assertEqual(table[idx_df.PULL_REQUESTS_IDX.values[0]][0], "pull_requests")
+            self.assertEqual(table[Tables.REPOSITORIES.value][0], "repositories")
+            self.assertEqual(table[Tables.USERNAMES.value][0], "usernames")
+            self.assertEqual(table[Tables.PULL_REQUESTS.value][0], "pull_requests")
 
 
 if __name__ == "__main__":
